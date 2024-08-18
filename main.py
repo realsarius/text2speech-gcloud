e0ec250301d497d1440ad6aed6295fbd8544632e
Berkan Sözer <berkansozer@outlook.com>
2024-08-18 21:38:35 +0300

    webview: added web page for handling PDF to speech conversion

--- main.py
+++ main.py
@@ -1,15 +1,61 @@
 from text_to_speech import TextToSpeechProcessor
 import os
 from dotenv import load_dotenv
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import uuid
 
 load_dotenv()
 
-if __name__ == "__main__":
-    pdf_file_path = "pdf/the-book-collector-example-2023-01-001.pdf"
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'pdf'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+def pdf_to_speech(pdf_file_path, output_gcs_uri):
     processor = TextToSpeechProcessor(
         pdf_path=pdf_file_path,
         project_id=os.getenv("PROJECT_ID"),
         location=os.getenv("LOCATION"),
-        output_gcs_uri=f"{os.getenv('OUTPUT_GCS_URI')}/output2.wav"
+        output_gcs_uri=output_gcs_uri
     )
     processor.process()
+
+@app.route("/upload", methods=['POST'])
+def upload_file():
+    if 'pdf_file' not in request.files:
+        return redirect(request.url)
+    
+    file = request.files['pdf_file']
+    
+    if file.filename == '':
+        return redirect(request.url)
+    
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        
+        unique_id = uuid.uuid4().hex
+        output_gcs_uri = f"{os.getenv('OUTPUT_GCS_URI')}/output_{unique_id}.wav"
+        
+        pdf_to_speech(file_path, output_gcs_uri)
+        
+        # Render the result template with message
+        return render_template('result.html', message=f'File successfully uploaded and processed. Output file: {output_gcs_uri}')
+    
+    return 'Invalid file type'
+
+@app.route("/")
+@app.route("/index")
+def home():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    app.run(debug=True)
+
